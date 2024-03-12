8a212ab3698a7de45298b83762a71289d97885af
NewRussianPirat <romaks1302@gmail.com>
2024-03-12 23:58:22 +0300

    Minor changes

--- Operations research/Task 5/main.cpp
+++ Operations research/Task 5/main.cpp
@@ -1,6 +1,5 @@
 #include <algorithm>
 #include <iostream>
-#include <queue>
 #include <vector>
 #include <utility>
 #include <iomanip>
@@ -46,12 +45,14 @@ void dfs(int begin, int target, const std::vector<bool> &criticalVertices, const
                 root->push_back(i);
             }
             dfs(EDGE_END, target, criticalVertices, criticalEdges, nonCriticalRoots, root);
-            root->pop_back();
+            if (root != nullptr && !root->empty()) {
+                root->pop_back();
+            }
         }
     }
 }
 
-int findCriticalRoot(int start, int finish, const std::vector<bool> &criticalVertices) {
+int findCriticalRootWeight(int start, int finish, const std::vector<bool> &criticalVertices) {
     if (start == finish) {
         return 0;
     }
@@ -60,7 +61,7 @@ int findCriticalRoot(int start, int finish, const std::vector<bool> &criticalVer
         int EDGE_BEGIN = edges[i].first.first;
         int EDGE_END = edges[i].first.second;
         if (EDGE_BEGIN == start && criticalVertices[EDGE_END]) {
-            result += findCriticalRoot(EDGE_END, finish, criticalVertices) + edges[i].second;
+            result += findCriticalRootWeight(EDGE_END, finish, criticalVertices) + edges[i].second;
             break;
         }
     }
@@ -97,28 +98,37 @@ void fillGraph() {
     vertices[0].second = std::min(vertices[2].second - edges[2].second, vertices[1].second - edges[1].second);
 }
 
-void Task1() {
-    std::cout << "Task 1" << std::endl;
-    const std::vector<int> t1 = {0, 15, 7, 5, 6, 8, 6, 10, 8, 9, 11, 10, 9};
-    setWeights(t1);
-    fillGraph();
-    std::vector<std::pair<int, int>> r(edges.size());
+std::vector<std::pair<int, int>> findTimeReserves() {
+    std::vector<std::pair<int, int>> r(NUMBER_OF_EDGES);
     for (int i = 0; i < edges.size(); ++i) {
         r[i].first = vertices[edges[i].first.second].second - vertices[edges[i].first.first].first - edges[i].second;
         r[i].second = vertices[edges[i].first.second].first - vertices[edges[i].first.first].second - edges[i].second;
     }
+    return r;
+}
+
+std::vector<bool> setCriticalVertices() {
     std::vector<bool> criticalVertices(NUMBER_OF_VERTICES, false);
     for (int i = 0; i < NUMBER_OF_VERTICES; ++i) {
         if (vertices[i].first == vertices[i].second) {
             criticalVertices[i] = true;
         }
     }
+    return criticalVertices;
+}
+
+std::vector<bool> setCriticalEdges(const std::vector<std::pair<int, int>> &timeReserves) {
     std::vector<bool> criticalEdges(NUMBER_OF_EDGES, false);
     for (int i = 0; i < NUMBER_OF_EDGES; ++i) {
-        if (r[i].first == 0 && r[i].second == 0) {
+        if (timeReserves[i].first == 0 && timeReserves[i].second == 0) {
             criticalEdges[i] = true;
         }
     }
+    return criticalEdges;
+}
+
+std::vector<std::vector<int>>
+findNonCriticalRoots(const std::vector<bool> &criticalVertices, const std::vector<bool> &criticalEdges) {
     std::vector<std::vector<int>> nonCriticalRoots;
     for (int i = 0; i < NUMBER_OF_VERTICES; ++i) {
         if (criticalVertices[i]) {
@@ -129,21 +139,30 @@ void Task1() {
             }
         }
     }
-    std::vector<int> R;
+    return nonCriticalRoots;
+}
+
+std::vector<double>
+findTensionCoefficient(const std::vector<std::vector<int>> &nonCriticalRoots, const std::vector<bool> &criticalVertices,
+                       std::vector<int> *R) {
     std::vector<double> N;
     for (auto i: nonCriticalRoots) {
         int start = edges[*i.begin()].first.first;
         int finish = edges[*(--i.end())].first.second;
-        int a = findCriticalRoot(start, finish, criticalVertices);
+        int a = findCriticalRootWeight(start, finish, criticalVertices);
         int b = 0;
         for (auto j: i) {
             b += edges[j].second;
         }
-        R.push_back(a - b);
+        R->push_back(a - b);
         N.push_back(double(b) / a);
     }
-    std::cout << "------------------------------------------------\n"
-              << " Event | Early Date | Late Date | Reserve Time |"
+    return N;
+}
+
+void showEventTable(const std::vector<bool> &criticalVertices) {
+    std::cout << "\n------------------------------------------------\n"
+              << " Event | Early Date | Late Date | Time Reserve |"
               << "\n------------------------------------------------\n";
     for (int i = 0; i < NUMBER_OF_VERTICES; ++i) {
         int n = 7;
@@ -158,12 +177,19 @@ void Task1() {
                   << "\n";
     }
     std::cout << "------------------------------------------------\n";
-    std::cout << "Critical vertices: ";
+}
+
+void showCriticalVertices(const std::vector<bool> &criticalVertices) {
+    std::cout << "\nCritical vertices: ";
     for (int i = 0; i < NUMBER_OF_VERTICES; ++i) {
         if (criticalVertices[i]) {
             std::cout << i << " ";
         }
     }
+    std::cout << "\n";
+}
+
+void showWorkTable(const std::vector<bool> &criticalEdges, const std::vector<std::pair<int, int>> &timeReserves) {
     std::cout << "\n------------------------------------------------------------------------\n"
               << "    Work     | Duration | Full Time Reserve | Independent Time Reserve |"
               << "\n------------------------------------------------------------------------\n";
@@ -179,17 +205,25 @@ void Task1() {
         }
         std::cout << "b" << i << " = (" << edges[i].first.first << ", " << edges[i].first.second << ")" << std::setw(n)
                   << "|" << std::setw(10) << edges[i].second
-                  << "|" << std::setw(19) << r[i].first
-                  << "|" << std::setw(26) << r[i].second
+                  << "|" << std::setw(19) << timeReserves[i].first
+                  << "|" << std::setw(26) << timeReserves[i].second
                   << "|\n";
     }
     std::cout << "------------------------------------------------------------------------\n";
-    std::cout << "Critical Edges: ";
+}
+
+void showCriticalEdges(const std::vector<bool> &criticalEdges) {
+    std::cout << "\nCritical Edges: ";
     for (int i = 0; i < NUMBER_OF_EDGES; ++i) {
         if (criticalEdges[i]) {
             std::cout << "b" << i << " ";
         }
     }
+    std::cout << "\n";
+}
+
+void showNonCriticalRootsTable(const std::vector<std::vector<int>> &nonCriticalRoots, const std::vector<int> &R,
+                               const std::vector<double> &N) {
     std::cout << "\n---------------------------------------------------------\n"
               << " Non-critical arcs | Time Reserve | Tension Coefficient |"
               << "\n---------------------------------------------------------\n";
@@ -202,6 +236,24 @@ void Task1() {
         std::cout << ") | " << std::setw(13) << R[i] << "|" << std::setw(21) << N[i] << "|\n";
     }
     std::cout << "---------------------------------------------------------\n";
+}
+
+void Task1() {
+    std::cout << "Task 1" << std::endl;
+    const std::vector<int> t1 = {0, 15, 7, 5, 6, 8, 6, 10, 8, 9, 11, 10, 9};
+    setWeights(t1);
+    fillGraph();
+    std::vector<std::pair<int, int>> timeReserves = findTimeReserves();
+    std::vector<bool> criticalVertices = setCriticalVertices();
+    std::vector<bool> criticalEdges = setCriticalEdges(timeReserves);
+    std::vector<std::vector<int>> nonCriticalRoots = findNonCriticalRoots(criticalVertices, criticalEdges);
+    std::vector<int> R;
+    std::vector<double> N = findTensionCoefficient(nonCriticalRoots, criticalVertices, &R);
+    showEventTable(criticalVertices);
+    showCriticalVertices(criticalVertices);
+    showWorkTable(criticalEdges, timeReserves);
+    showCriticalEdges(criticalEdges);
+    showNonCriticalRootsTable(nonCriticalRoots, R, N);
 }
 
 void Task2() {
